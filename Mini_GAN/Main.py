14dcbcc2aa1143c9a1b852589abd4a205dd67d7a
alicejl <46697092+alicejl@users.noreply.github.com>
2019-11-18 00:08:23 -0500

    added to cuda code which still requires testing and integration on cloud

--- Mini_GAN/Main.py
+++ Mini_GAN/Main.py
@@ -20,22 +20,11 @@ def weights_init(m):
         m.bias.data.fill_(0)
 '''
 
-def train():
-    image_path = 'C:/Users/Alice/Documents/School/ECE324/Project/tiny-imagenet-200/tiny-imagenet-200/val/'
-    # Settings and Hyperparameters
-    fresh_start = False  # True to start new model False to continue training saved model
-    d_import_name = 'good_d.pt'
-    g_import_name = 'baseline.pt'
-    d_export_name = 'good_d.pt'
-    g_export_name = 'good_g.pt'
-    g_error_scaler = 0.5
-    g_train_scaler = 1
-    num_images = 1000
-    num_epochs = 50
-    print_interval = 1
-    batch_size = 64
+def train(image_path, num_images, num_epochs, error_scaler=0.5, training_scaler=1, fresh_start = True,
+          d_import_name='discriminator.pt', g_import_name='generator.pt', d_export_name='discriminator.pt',
+          g_export_name='generator.pt', batch_size=64, print_interval=1, save=False):
 
-    # Model Parameters
+    # Model HyperParameters
     g_hidden_size1 = 32
     g_hidden_size2 = 16
     d_input_size = 64    # dimension of square image
@@ -50,15 +39,13 @@ def train():
     sgd_momentum = 0.9
 
     d_steps = 1
-    g_steps = g_train_scaler
+    g_steps = training_scaler
 
     if fresh_start:
         G = Generator()
-        G.float()
         D = Discriminator(input_size=d_input_size, kernelSize=d_kernel_size, kernelNum=d_kernel_number,
                       hidden_size=d_hidden_size, output_size=d_output_size, convlayers=d_conv_layers,
                       fclayers=d_fclayers)
-        D.float()
         D.apply(weights_init)
         G.apply(weights_init)
     else:
@@ -67,20 +54,18 @@ def train():
             G.load_state_dict(torch.load(g_import_name))
             G.eval()'''
             G = torch.load(g_import_name)
-            print('imported')
-            D = Discriminator(input_size=d_input_size, kernelSize=d_kernel_size, kernelNum=d_kernel_number,
-                              hidden_size=d_hidden_size, output_size=d_output_size, convlayers=d_conv_layers,
-                              fclayers=d_fclayers)
-            D.float()
-            #D = torch.load(d_import_name)
+            D = torch.load(d_import_name)
+            print(g_import_name, 'and', d_import_name, 'imported')
         except:
             G = Generator()
-            G.float()
             D = Discriminator(input_size=d_input_size, kernelSize=d_kernel_size, kernelNum=d_kernel_number,
                               hidden_size=d_hidden_size, output_size=d_output_size, convlayers=d_conv_layers,
                               fclayers=d_fclayers)
-            D.float()
-
+    if torch.cuda.is_available():
+        G.cuda()
+        D.cuda()
+    G.float()
+    D.float()
     criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
     criterion2 = nn.MSELoss()
     d_optimizer = optim.Adam(D.parameters(), lr=d_learning_rate)  #, momentum=sgd_momentum)
@@ -135,7 +120,7 @@ def train():
                 dg_fake_decision = D(g_fake_data)
                 g_error_1 = criterion(dg_fake_decision.squeeze(), torch.zeros([dg_fake_decision.shape[0]]))  # Train G to pretend it's genuine
                 g_error_2 = criterion2(g_fake_data, real)
-                g_error = g_error_1 + g_error_scaler * g_error_2
+                g_error = g_error_1 + error_scaler * g_error_2
                 g_error.backward()
                 g_optimizer.step()  # Only optimizes G's parameters'''
 
@@ -144,9 +129,9 @@ def train():
 
         if epoch % print_interval == 0:
             print("(", time() - t_init, ") Epoch", epoch, ": D (error:", d_loss[d_loss_index - 1], ") G (error:", g_loss[g_loss_index - 1], "); ")
-
-    torch.save(D, d_export_name)
-    torch.save(G, g_export_name)
+    if save:
+        torch.save(D, d_export_name)
+        torch.save(G, g_export_name)
 
     '''if g_steps==1:
         fig, (ax1, ax2) = plt.subplots(2, sharex=True)
@@ -178,4 +163,18 @@ def train():
     display_processed_imgs(gen_input.detach(), g_fake_data.detach(), 4)
 
 
-train()
+if torch.cuda.is_available():
+    torch.set_default_tensor_type(torch.cuda.FloatTensor)
+
+image_path = 'C:/Users/Alice/Documents/School/ECE324/Project/tiny-imagenet-200/tiny-imagenet-200/val/'
+d_import_name = 'good_d.pt'
+g_import_name = 'baseline.pt'
+d_export_name = 'good_d.pt'
+g_export_name = 'good_g.pt'
+num_images = 1000
+num_epochs = 0
+print_interval = 1
+batch_size = 64
+train(image_path, num_images, num_epochs, error_scaler=0.5, training_scaler=1, fresh_start = True,
+          d_import_name='discriminator.pt', g_import_name='generator.pt', d_export_name='discriminator.pt',
+          g_export_name='generator.pt', batch_size=64, print_interval=1, save=False)
